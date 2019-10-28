678d4b316bacbf190859485129938f6e021404d4
matzar <mat.zar@icloud.com>
2019-10-28 17:59:29 +0000

    replacing seq column non-consecutive numbers with consecutive numbers

--- Statistics/StudentGoals.R
+++ Statistics/StudentGoals.R
@@ -3,11 +3,11 @@
 library(tidyverse)
 
 # read csv file (worse variable recognition)
-# f <- "data/StudentGoalsData.csv"
-# StudentGoalsData <- read_csv(f)
+f <- "data/StudentGoalsData.csv"
+StudentGoalsData <- read_csv(f)
 
 # read csv file
-StudentGoalsData <- data.table::fread("data/StudentGoalsData.csv") # fread - better variable recognition
+# StudentGoalsData <- data.table::fread("data/StudentGoalsData.csv") # fread - better variable recognition
 
 # clean data
 CleanedStudentGoalsData <- drop_na(StudentGoalsData)
@@ -19,15 +19,37 @@ write_csv(CleanedStudentGoalsData, "data/CleanedStudentGoalsData.csv")
 View(StudentGoalsData)
 View(CleanedStudentGoalsData)
 
+# save CleanedStudentGoalsData table in a simple variable called dat
+dat <- CleanedStudentGoalsData
+
+# after cleaning seq column contains non-consecutive numbers 
+# replace seq column's numbers with consequetive numbers 
+dat$seq <- 1:nrow(dat)
+View(dat)
+
 # check the data's structure
 str(CleanedStudentGoalsData) # uses int
+head(CleanedStudentGoalsData)
+str(dat)
 
-# 
+View(dat)
+# plot data
+data <- CleanedStudentGoalsData
+plot(data[data$year])
+plot(data[data$year, ]$seq)
+s <- data[data$year, ]$seq
 
-
-# plot 
+# ggplot data
 # ggplot(mpg, aes(displ, hwy, colour = class)) +
 # geom_point()
 # s <- ggplot(CleanedStudentGoalsData, fill=drv)
-# s + geom_bar()
+# s + geom_point()
+
+# playground
+x <- c(1, 2, 3, 4)
+x < 10
+CleanedStudentGoalsData[, 3]
+colnames(CleanedStudentGoalsData)
+CleanedStudentGoalsData$year
+class(CleanedStudentGoalsData$year) # returns a vector
 
