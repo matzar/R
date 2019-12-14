ece838191b90d6cd986cd9bbd52ba2ee64d57908
matzar <mat.zar@icloud.com>
2019-12-14 14:37:55 +0000

    r studio graphs working

--- Networks/Network/Network-Analysis.R
+++ Networks/Network/Network-Analysis.R
@@ -1,32 +1,31 @@
-install.packages("igraph", dependencies=TRUE, repos='http://cran.rstudio.com/')
-
-options(download.file.method = "wget")
-install.packages("igraph")
-library(igraph)
-
-
 # download
-pth <- "http://nrvis.com/download/data/misc/actor-movie.zip"
-download.file(pth, destfile = "actor-movie.zip")
+pth <- "http://nrvis.com/download/data/rt/rt-pol.zip"
+download.file(pth, destfile = "rt-pol.zip")
 
 # see file names
-unzip("actor-movie.zip", list = TRUE)
+# unzip("socfb-Berkeley13.zip", list = TRUE)
 
 # unzip
-unz <- unzip("actor-movie.zip", "actor-movie.edges")
+unz <- unzip("rt-pol.zip", "rt-pol.txt")
+
 
 # quick look : looks like edge list
 readLines(unz, n=10)
 
 # skip first line to avoid % bipartite unweighted" 
-dat <- read.table(unz, skip=1, sep=",")
+# dat <- read.table(unz, skip=1, sep=",")
+dat <- read.table(unz, sep=",")
 
 # look
 head(dat)
 str(dat)
 
+# load as a graph
+library(igraph)
+
 g <- graph_from_data_frame(dat)
 g
 
-# References 
-# https://stackoverflow.com/questions/58350400/how-to-read-in-edges-file-in-r
+summary(g)
+
+
